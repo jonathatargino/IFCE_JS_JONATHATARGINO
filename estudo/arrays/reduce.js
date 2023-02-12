1352c2a1e70bd81e953b57aa9f28e3e75c3b791b
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-11 21:29:38 -0300

    Filter + Map + Reduce

--- estudo/arrays/reduce.js
+++ estudo/arrays/reduce.js
@@ -17,3 +17,19 @@ const novoArray = numeros.reduce((acumulador, valor, indice, array) => {
 }, [])
 // Output: [ 25, 2500, 6400, 1, 4, 9, 16, 25, 64 ]
 console.log(novoArray);
+
+// Desafio Filter + Map + Reduce:
+// -> Filtrar pares
+// -> Dobrar os valores
+// -> Reduzir (somar tudo)
+
+const num = [5, 50, 80, 1, 2, 3, 4, 5 , 8, 7, 11, 15, 22, 27]
+
+const pares = num.filter(num => num % 2 == 0);
+const paresDobrados = pares.map(par => par * 2);
+const somaParesDobrados = paresDobrados.reduce((acumulador, valor) => {
+  return acumulador += valor;
+}, 0)
+
+// Output: 332
+console.log(somaParesDobrados);
