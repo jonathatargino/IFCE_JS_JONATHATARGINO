8cd5f83e1c6f440b419087d69b4cae0eebbfa29a
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-12 18:36:54 -0300

    Getters e Setters

--- estudo/classes/criacao.js
+++ estudo/classes/criacao.js
@@ -26,3 +26,47 @@ const p1 = new Pessoa("Jonatha", "Targino");
 // Output: Pessoa { nome: 'Jonatha', sobrenome: 'Targino'}
 console.log(p1);
 
+// Getters e Setters em classes
+
+// Symbol serve para criar variáveis privadas. 
+const _velocidade = Symbol('velocidade')
+
+class Carro {
+  constructor(nome){
+    this.nome = nome;
+    this[_velocidade] = 0;
+  }
+
+  get velocidade(){
+    return this[_velocidade]
+  }
+
+  set velocidade(vel){
+    if (typeof vel !== 'number') return;
+    if (vel < 0 || vel > 100) return;
+
+    this[_velocidade] = vel;
+  }
+
+  acelerar(){
+    if (this[_velocidade] < 100) this[_velocidade]++
+  }
+  freiar(){
+    if (this[_velocidade] > 0) this[_velocidade]--
+  }
+}
+
+const c1 = new Carro('Fusca');
+
+// Output: Carro { nome: 'Fusca', [Symbol(velocidade)]: 0 }
+console.log(c1);
+
+// Acessando velocidade através do get:
+// Output: 0
+console.log(c1.velocidade)
+
+c1.velocidade = 99
+console.log(c1.velocidade) // _velocidade recebe 99
+
+c1.velocidade = 140
+console.log(c1.velocidade) // _velocidade continua 99
