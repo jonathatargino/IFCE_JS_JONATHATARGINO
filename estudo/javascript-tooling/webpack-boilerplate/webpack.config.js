c856108eebfa9b78745005a697614240f332fb55
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-02-15 15:35:01 -0300

    Webpack (Boilerplate)

---
+++ estudo/javascript-tooling/webpack-boilerplate/webpack.config.js
@@ -0,0 +1,27 @@
+// Configurações do Webpack
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  // arquivo de entrada
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  // Configuração de loaders
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map',
+}
