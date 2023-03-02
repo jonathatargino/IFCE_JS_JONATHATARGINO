1c957ea31e08aa3a974e2f722a126e4d5a70c790
Jonatha Targino <j.oliveiratargino@hotmail.com>
2023-03-02 15:55:45 -0300

    Tipo object (objetos em geral)

---
+++ estudo/typescript/src/obj.ts
@@ -0,0 +1,21 @@
+const objetoA = {
+  chaveA: "Valor A",
+  chaveB: "Valor B",
+};
+
+objetoA.chaveA = "Outro valor";
+// Não podemos criar um nova nova propriedade para um objeto (sem index signature).
+objetoA.chaveC = "Nova Chave";
+
+// Tipando um objeto
+const objetoB: {
+  chaveA: string;
+  chaveB: string;
+  chaveC?: string;
+  [key: string]: unknown; // index signature -> nos permite criar uma nova propriedade para um objeto
+} = {
+  chaveA: "Valor A",
+  chaveB: "Valor B",
+};
+
+objetoB.chaveD = "Chave D";
